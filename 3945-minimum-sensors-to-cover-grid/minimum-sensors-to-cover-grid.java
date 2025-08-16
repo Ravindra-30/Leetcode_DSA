66d4b15b4d8988682134f4dce9c090145e77b9c9
Ravindra <67945532+Ravindra-30@users.noreply.github.com>
2025-08-16 22:32:23 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (69.59%) - LeetSync

--- 3945-minimum-sensors-to-cover-grid/minimum-sensors-to-cover-grid.java
+++ 3945-minimum-sensors-to-cover-grid/minimum-sensors-to-cover-grid.java
@@ -1,8 +1,8 @@
 class Solution {
     public int minSensors(int n, int m, int k) {
         int cover = (2 * k) + 1;
-        int h = (int)Math.ceil(((double)n) / cover);
-        int v = (int)Math.ceil(((double)m) / cover);
+        int h = (n/cover) + (n % cover > 0? 1: 0); 
+        int v = (m/cover) + (m % cover > 0? 1: 0); 
         return h * v;
     }
 }
